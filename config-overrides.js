17cd528e873e397995c26b7bddc10ce5486c8359
Bisheng 007 <bs007@xuweijian.local>
2018-10-07 15:59:15 +0800

    create this project by using create-react-app;configure it with electron

---
+++ config-overrides.js
@@ -0,0 +1,6 @@
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+    // console.log(config, env);
+    // throw new Error('****print config and env*****')
+    return config;
+}
